f9b2ee16818dd90727969e4b7d329ceb2791c5d6
lindo315 <lindodlamini315@gmail.com>
2024-01-21 10:17:23 +0200

    First Push
    Created an array of random numbers in a function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+// Each box generates a number within a set array
+const numbers = [100, 250, 220, 780, 400, 80, 520, 880, 120, 20];
+
+// Function to generate a random number from the array
+function getRandomNumber() {
+  // Generate a random index within the array length
+  const randomIndex = Math.floor(Math.random() * numbers.length);
+
+  // Return the element at the randomly generated index
+  return numbers[randomIndex];
+}
+
+const randomNum = getRandomNumber();
+console.log(randomNum);
